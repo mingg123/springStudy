feb928264ec8b07407b05b3841d5697bd4e6ccb9
minji_kim2 <minji_kim2@tmax.co.kr>
2021-07-19 10:22:36 +0900

    feat : add restApi post example 1. client, server 서버 두개 띄워서 post api 사용 2. client는 8082, 서버는 9091

--- client/src/main/java/com/example/client/controller/ApiController.java
+++ client/src/main/java/com/example/client/controller/ApiController.java
@@ -1,22 +1,29 @@
 package com.example.client.controller;
 
+import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
 import com.example.client.service.RestTemplateService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/client/")
+@RequestMapping("/api/client")
 public class ApiController {
     
     @Autowired
     private RestTemplateService restTemplateService;
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public UserResponse getHello() {
         return restTemplateService.hello();
+    }
+
+    @PostMapping("/hello")
+    public UserResponse postHello() {
+        return restTemplateService.post();
     }
 }
--- client/src/main/java/com/example/client/service/RestTemplateService.java
+++ client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -2,6 +2,7 @@ package com.example.client.service;
 
 import java.net.URI;
 
+import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
 
 import org.springframework.http.ResponseEntity;
@@ -25,5 +26,31 @@ public class RestTemplateService {
         System.out.println(result.getStatusCode());
         System.out.println(result.getBody());
         return result.getBody();
+    }
+
+    public UserResponse post() {
+        //http://localhost:9091/api/server/user/{userId}/name/{userName}
+        URI uri = UriComponentsBuilder.fromUriString("http://localhost:9091").path("/api/server/user/{userId}/name/{userName}")
+        .encode()
+        .build()
+        .expand(100, "mingg")
+        .toUri();
+
+        System.out.println(uri);
+
+        //http body -> 
+        UserRequest req = new UserRequest();
+        req.setName("minji");
+        req.setAge(26);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        //응답은 이걸로 받을거야 
+        ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
+        return response.getBody();
     }
 }
