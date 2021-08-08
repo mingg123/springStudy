755594413cb95b5a47e1097f03a69fb1fc1f6a49
minji_kim2 <minji_kim2@tmax.co.kr>
2021-08-08 18:59:57 +0900

    feat : basic h2 Database 1. lombok, h2 datajbase connection, server port 8070

---
+++ bookmanager/src/main/java/com/jps/bookmanager/domain/User.java
@@ -0,0 +1,32 @@
+package com.jps.bookmanager.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+// @Setter
+// @Getter
+// @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+// @EqualsAndHashCode
+@Data
+@Builder
+public class User {
+    @NonNull
+    private String name;
+    @NonNull
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
